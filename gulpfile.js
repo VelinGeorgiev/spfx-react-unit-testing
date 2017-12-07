f3fa17a5dc555d1e3151f16784083741136844d2
Velin Georgiev <velin.georgiev@gmail.com>
2017-12-07 22:53:55 +0000

    webpack analyzer added

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,25 @@
 
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
+const path = require('path');
+const bundleAnalyzer = require('webpack-bundle-analyzer');
+
+// https://github.com/SharePoint/sp-dev-docs/blob/master/docs/spfx/toolchain/optimize-builds-for-production.md
+build.configureWebpack.mergeConfig({
+    additionalConfiguration: (generatedConfiguration) => {
+        const lastDirName = path.basename(__dirname);
+        const dropPath = path.join(__dirname, 'temp', 'stats');
+        generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
+            openAnalyzer: false,
+            analyzerMode: 'static',
+            reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+            generateStatsFile: true,
+            statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
+            logLevel: 'error'
+        }));
+
+        return generatedConfiguration;
+    }
+});
 
 build.initialize(gulp);
